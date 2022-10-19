0b2e88dda7a93f723043d08896e23d7cd7f780ad
AlexSichkar <116178242+AlexSichkar@users.noreply.github.com>
2022-10-20 01:31:25 +0300

    my_review

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery ball1 = new Lottery();  //ToDo use "for" loop when create few objects of class, in this task you can create one Lottery object with three Balls if you want
+        System.out.println(ball1.getRandomBall().toString());   //ToDo with "for" loop lines 6,8,10 (and lines 5,7,9) can replace by one line
+        Lottery ball2 = new Lottery();
+        System.out.println(ball2.getRandomBall().toString());   //ToDo dont forget about constant variables for magic numbers
+        Lottery ball3 = new Lottery();
+        System.out.println(ball3.getRandomBall().toString());
     }
 }
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+//ToDo this empty line is redundant
+//ToDo this empty line is redundant
+    public static String getRandomColor() { //ToDo better create new class object (with necessary variables and methods) than use static method
+        int index = new Random().nextInt(Colors.values().length);
+        return Colors.values()[index].toString();   //ToDo use name() for getting String representation of enum constant
     }
 }
